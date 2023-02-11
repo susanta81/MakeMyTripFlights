4b5b255cd87d5b17a6d1934df1f80f31f33b6e6c
susanta81 <124057990+susanta81@users.noreply.github.com>
2023-02-11 20:29:25 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/suspro/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/suspro/HelloWorld/HelloWorldApplication.java
@@ -21,7 +21,7 @@ public class HelloWorldApplication {
 		}catch(UnknownHostException ex){
 			ex.printStackTrace();
 		}
-		return "Welcome to Susanta Flight Service V21 : "+ip;
+		return "Welcome to Susanta Flight Service V22 : "+ip;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
